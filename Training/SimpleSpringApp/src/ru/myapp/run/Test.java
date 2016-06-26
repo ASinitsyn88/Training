dc360f580a1ccdaab6310c3c4cb2f16044004fdd
ASinitsyn88 <sinitsyn88@gmail.com>
2016-06-26 20:37:25 +0300

    Modified SimpleSpringApp

--- Training/SimpleSpringApp/src/ru/myapp/run/Test.java
+++ Training/SimpleSpringApp/src/ru/myapp/run/Test.java
@@ -1,19 +1,16 @@
 package ru.myapp.run;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.myapp.bean.Student;
 
 public class Test {
 
     public static void main(String[] args) {
 
-        Resource resource = new ClassPathResource("applicationContext.xml");
-        BeanFactory factory = new XmlBeanFactory(resource);
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Student student = (Student)factory.getBean("studentbean");
+        Student student = (Student)context.getBean("studentbean");
         student.displayInfo();
     }
 }

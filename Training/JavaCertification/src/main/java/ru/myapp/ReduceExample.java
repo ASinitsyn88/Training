5b7fefeda51d75431f0005e1b3bcdfc1cf64a153
as.sinitsyn <as.sinitsyn@jet.msk.su>
2018-11-27 11:05:24 +0300

    Пример редукции на основе массива и списка

--- Training/JavaCertification/src/main/java/ru/myapp/ReduceExample.java
+++ Training/JavaCertification/src/main/java/ru/myapp/ReduceExample.java
@@ -1,5 +1,8 @@
 package ru.myapp;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 /**
@@ -8,11 +11,20 @@ import java.util.stream.Stream;
 public class ReduceExample {
 
 	public static void main(String[] args) {
+		// Пример редукции на основе массива
+		Integer[] intArr = {1, 2, 3};
 		// Создать Stream<Integer>
-		int sum = Stream.of(1, 2, 3)
+		int sum = Stream.of(intArr)
 				// Присвоить аккумулятору начальное значение 0. Затем сложить аккумулятор с каждым элементом
 				.reduce(0, (acc, element) -> acc + element);
-
 		System.out.println(sum);
+
+		// Пример редукции на основе списка
+		List<Integer> intList = new ArrayList<>(Arrays.asList(1, 2, 3));
+		// Создать Stream<Integer>
+		int sum2 = Stream.of(intList.toArray(new Integer[0]))
+				// Присвоить аккумулятору начальное значение 0. Затем сложить аккумулятор с каждым элементом
+				.reduce(0, (acc, element) -> acc + element);
+		System.out.println(sum2);
 	}
 }

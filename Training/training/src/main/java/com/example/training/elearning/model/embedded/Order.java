e1087acf7964e86c3947bf92956702a2c757a33f
a.sinitsyn <sinitsyn88@gmail.com>
2024-06-30 19:26:08 +0100

    Composite key example

---
+++ Training/training/src/main/java/com/example/training/elearning/model/embedded/Order.java
@@ -0,0 +1,21 @@
+package com.example.training.elearning.model.embedded;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(schema = "e_learning", name = "_order")
+public class Order {
+    // Composite key example
+    @EmbeddedId
+    private OrderId id;
+    private String orderInfo;
+    private String anotherField;
+}

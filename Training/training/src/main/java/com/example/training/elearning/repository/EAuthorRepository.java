4871572c449137ec425affcc2e27d536d3dc1e32
a.sinitsyn <sinitsyn88@gmail.com>
2024-07-13 22:27:14 +0100

    Custom repository methods example

--- Training/training/src/main/java/com/example/training/elearning/repository/EAuthorRepository.java
+++ Training/training/src/main/java/com/example/training/elearning/repository/EAuthorRepository.java
@@ -1,8 +1,23 @@
 package com.example.training.elearning.repository;
 
 import com.example.training.elearning.model.Author;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
 public interface EAuthorRepository extends CrudRepository<Author, Integer> {
 
+    @Modifying
+    @Transactional
+    @Query("update Author a set a.age = :age where a.id = :id")
+    int updateAuthor(int age, int id);
+
+    List<Author> findAllByFirstName(String fn);
+    List<Author> findAllByFirstNameIgnoreCase(String fn);
+    List<Author> findAllByFirstNameContainingIgnoreCase(String fn);
+    List<Author> findAllByFirstNameStartsWithIgnoreCase(String fn);
+    List<Author> findAllByFirstNameEndsWithIgnoreCase(String fn);
+    List<Author> findAllByFirstNameInIgnoreCase(List<String> firstNames);
 }
